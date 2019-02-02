cce39dba2ecf8376de743fd58eb4e05bc5307899
DragonFury24 <dennisdnguyen19@gmail.com>
2019-02-02 09:18:04 -0500

    Setup DrawView, Painter, and Canvas

---
+++ app/src/main/java/com/example/audiome_android/DrawView.java
@@ -0,0 +1,23 @@
+package com.example.audiome_android;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class DrawView extends View {
+    public DrawView(Context context) {
+        super(context);
+    }
+
+    private Paint paint = new Paint();
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        paint.setColor(Color.RED);
+        paint.setTextSize(100);
+        canvas.drawText("Hello world", (float)(getWidth() / 2), (float)(getHeight() / 2), paint);
+    }
+}
